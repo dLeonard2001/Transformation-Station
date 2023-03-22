7bb8e7f2456f3e13a651a8d76777e1c2f8dabcb6
Jason Casareno <jcasareno@csumb.edu>
2023-03-21 17:34:55 -0700

    Improved 3D Axis Visualization

---
+++ Transformation-Station/Assets/Scripts/GridRenderer.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class GridRenderer : MonoBehaviour
+{
+    public float lineThickness = 0.1f;
+    public float axisLength = 1f;
+
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        
+        lineRenderer.startWidth = lineRenderer.endWidth = lineThickness;
+        
+        lineRenderer.transform.localPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        var position = transform.position;
+        
+        lineRenderer.SetPosition(0, position - (Vector3.right * axisLength / 2f));
+        lineRenderer.SetPosition(1, position + (Vector3.right * axisLength / 2f));
+        lineRenderer.SetPosition(2, position);
+
+        lineRenderer.SetPosition(3, position - (Vector3.up * axisLength / 2f));
+        lineRenderer.SetPosition(4, position + (Vector3.up * axisLength / 2f));
+        lineRenderer.SetPosition(5, position);
+        
+        lineRenderer.SetPosition(6, position - (Vector3.forward * axisLength / 2f));
+        lineRenderer.SetPosition(7, position + (Vector3.forward * axisLength / 2f));
+        lineRenderer.SetPosition(8, position);
+    }
+}
