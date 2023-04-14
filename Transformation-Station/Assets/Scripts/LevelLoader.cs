2ad6366d895870fe21cd2d517daffb480adffd31
Dylan L <dleonard@csumb.edu>
2023-04-14 13:01:03 -0700

    Added sounds to all scenes

--- Transformation-Station/Assets/Scripts/LevelLoader.cs
+++ Transformation-Station/Assets/Scripts/LevelLoader.cs
@@ -9,11 +9,21 @@ public class LevelLoader : MonoBehaviour
     [SerializeField] private Animator transition;
     [SerializeField] private int transitionTime = 1;
     
-    private static readonly int Start = Animator.StringToHash("Start");
+    private static readonly int fadeIn = Animator.StringToHash("Start");
     
     [SerializeField] private AudioSource sceneStartSource;
 
     [SerializeField] private AudioClip sceneStartSound;
+
+    [SerializeField] private bool playStartSound;
+
+    private void Start()
+    {
+        // play the "SceneStart" sound
+        
+        if(playStartSound) 
+            sceneStartSource.PlayOneShot(sceneStartSound);
+    }
 
     public void LoadLevel(string sceneName)
     {
@@ -28,15 +38,11 @@ public class LevelLoader : MonoBehaviour
             yield break;
         }
         
-        transition.SetTrigger(Start);
+        transition.SetTrigger(fadeIn);
 
         yield return new WaitForSeconds(transitionTime);
 
         SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
-        
-        yield return new WaitForSeconds(2.0f);
-        
-        // play the "SceneStart" sound
-        sceneStartSource.PlayOneShot(sceneStartSound);
+
     }
 }
