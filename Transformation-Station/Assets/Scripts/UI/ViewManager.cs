5e29d724cbbe803cf5df8c388acd2ab4e5472ab8
Dylan Leonard <dleonard@csumb.edu>
2023-04-27 21:13:47 -0700

    Revert "Merge pull request #38 from dLeonard2001/main-menu-naya-2"
    This reverts commit ac69b9f36b81f1cf65bb9ae7256ecc2eebd660dc, reversing
    changes made to 3212844716c2bcda5ed08ca1514aff2e19e148e9.

--- Transformation-Station/Assets/Scripts/UI/ViewManager.cs
+++ Transformation-Station/Assets/Scripts/UI/ViewManager.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 
 public class ViewManager : MonoBehaviour
 {
+    [SerializeField] private float animDuration;
     [SerializeField] private Camera cam;
-    
+
+    private bool crIsActive;
+
     // Update is called once per frame
     void LateUpdate()
     {
@@ -21,24 +24,51 @@ public class ViewManager : MonoBehaviour
         switch (direction.name)
         {
             case "front":
-                cam.transform.localEulerAngles = new Vector3(0f, -180, 0f);
+                StartCoroutine(ViewChanger(new Vector3(0, -180, 0)));
                 break;
             case "back":
-                cam.transform.localEulerAngles = new Vector3(0f, 0, 0f);
+                StartCoroutine(ViewChanger(new Vector3(0f, 0, 0f)));
                 break;
             case "right":
-                cam.transform.localEulerAngles = new Vector3(0f, -90, 0f);
+                StartCoroutine(ViewChanger(new Vector3(0f, -90, 0f)));
                 break;
             case "left":
-                cam.transform.localEulerAngles = new Vector3(0f, 90, 0f);
+                StartCoroutine(ViewChanger(new Vector3(0f, 90, 0f)));
                 break;
             case "top":
-                cam.transform.localEulerAngles = new Vector3(90, 0f, 0f);
+                StartCoroutine(ViewChanger(new Vector3(90, 0f, 0f)));
                 break;
             case "bottom":
-                cam.transform.localEulerAngles = new Vector3(-90, 0f, 0f);
+                StartCoroutine(ViewChanger(new Vector3(-90, 0f, 0f)));
                 break;
         }
+    }
+
+    private IEnumerator ViewChanger(Vector3 eulerAngles)
+    {
+        crIsActive = true;
+        
+        float elapsedTime = 0;
+        float time = elapsedTime / animDuration;
+
+        float camX = cam.transform.localEulerAngles.x;
+        float camY = cam.transform.localEulerAngles.y;
+        float camZ = cam.transform.localEulerAngles.z;
+
+        while (time <= 1)
+        {
+            
+            elapsedTime += Time.deltaTime;
+            time = elapsedTime / animDuration;
+            
+            cam.transform.localEulerAngles =
+                new Vector3(Mathf.Lerp(camX, eulerAngles.x, time),
+                    Mathf.Lerp(camY, eulerAngles.y, time),
+                    Mathf.Lerp(camZ, eulerAngles.z, time));
+            yield return null;
+        }
+
+        crIsActive = false;
     }
 
     private void OnMouseDown()
